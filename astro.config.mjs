22af4d176f52483775d6f8261b4601abaff83a58
Armand Collier <schneideretcollier.innovation@gmail.com>
2026-05-07 04:40:49 +0200

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://armandcollier.com',
+  site: 'https://armandParser.github.io',
+  base: '/lesitedarmand',
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'viewport',
